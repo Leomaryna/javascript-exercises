0efcce54cc7c2ba863c2c477e8a8148cf71c5ce8
Leomaryna <leomaryna@gmail.com>
2023-03-10 19:31:14 +0100

    solve first 3 exercises on javascript - helloWorld - repeat a String - reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repetition = "";
+    for (let i = 0; i < num; i++) {
+        repetition += string;
+    }
+    return repetition;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let emptyString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        emptyString += str.charAt(i);
+    }
+    return emptyString;
 };
 
 // Do not edit below this line
